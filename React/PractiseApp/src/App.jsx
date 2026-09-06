6790b5d20753ee2e98c1e61c715b85c1496da0bb
ZulfiqarAli111 <zulfiqarali.bscssef20@iba-suk.edu.pk>
2026-09-06 12:25:25 +0500

    react, jsx, components

---
+++ React/PractiseApp/src/App.jsx
@@ -0,0 +1,10 @@
+import MyButton from "./MyButton";
+
+function App(){
+  return <div>
+    <h1>This is React Practise Project</h1>
+    <MyButton/>
+  </div>
+}
+
+export default App;
---
+++ React/PractiseApp/src/MyButton.jsx
@@ -0,0 +1,7 @@
+// This is Component That can be resused any where
+// so component are the resuable UI code that are rendered on application
+function MyButton(){
+    return <button>Click Me</button>
+}
+
+export default MyButton;
