8fed7f81efa5d79533eee6e48870a5fbe625d2be
ZulfiqarAli111 <zulfiqarali.bscssef20@iba-suk.edu.pk>
2026-09-07 13:07:30 +0500

    react with bootstrap

--- React/PractiseApp/src/App.jsx
+++ React/PractiseApp/src/App.jsx
@@ -1,9 +1,11 @@
 import MyButton from "./MyButton";
+import MyComponent from "./MyComponent";
 
 function App(){
   return <div>
     <h1>This is React Practise Project</h1>
     <MyButton/>
+    <MyComponent/>
   </div>
 }
 
---
+++ React/PractiseApp/src/MyComponent.jsx
@@ -0,0 +1,10 @@
+
+const MyComponent = () => {
+  return (
+    <div>
+        <a href="#" className='btn btn-primary'>Google</a>
+    </div>
+  )
+}
+
+export default MyComponent
