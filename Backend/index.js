c8435d32b36103798d5a1879c399db8b22f86da0
ZulfiqarAli111 <zulfiqarali.bscssef20@iba-suk.edu.pk>
2026-09-15 20:27:44 +0500

    creating server and routes in node js

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,19 @@
-console.log(2+30);
-for(let i=0;i<10;i++){
-    console.log(i+1);
-}
+// creating server in node js
+import http from 'http'
+
+const server = http.createServer((req,res)=>{
+    if(req.url === "/"){
+        res.end("Welcome to Home")
+    }
+    else if(req.url === "/about"){
+        res.end("Welcome to about page")
+    }
+    else{
+        res.end("404 Page not Found")
+    }
+})
+
+server.listen(8000,()=>{
+    console.log("server is started")
+});
+
