a9ebb8cf6c6d8e792c412026524ca9d3a1ce0af5
ZulfiqarAli111 <zulfiqarali.bscssef20@iba-suk.edu.pk>
2026-09-20 15:05:09 +0500

    login and logout api done

--- Authentication/backend/controllers/user.controller.js
+++ Authentication/backend/controllers/user.controller.js
@@ -35,8 +35,6 @@ export const singup = async (req,res) =>{
             maxAge:7*24*60*60*1000,
         })
 
-
-
         return res.status(201).json({user:{
             firstName,
             lastName,
@@ -44,11 +42,57 @@ export const singup = async (req,res) =>{
             email,
         }})
 
+    } catch (error) {
 
+        return res.status(500).json({message:"Internal Sever Error"});
+    }
+}
+
+
+export const login = async(req,res)=>{
+    try {
+        
+        const {email,password} = req.body;
+
+        const existUser = await User.findOne({email});
+        if(!existUser){
+            return res.status(400).json({message:"User does not Exist"});
+        }
+
+        let passwordMatch = await bcrypt.compare(password,existUser.password);
+
+        if(!passwordMatch){
+            return res.status(400).json({message:"Incorrect Password"});
+        }
+
+        const jwtToken = generateToken(existUser._id);
+
+        res.cookie("token",jwtToken,{
+            httpOnly:true,
+            secure:process.env.NODE_ENVIRONMENT == "production",
+            sameSite:"strict",
+            maxAge:7*24*60*60*1000,
+        })
+
+        return res.status(201).json({user:{
+            firstName:existUser.firstName,
+            lastName:existUser.lastName,
+            userName:existUser.userName,
+            email:existUser.email,
+        }})
 
 
     } catch (error) {
+        return res.status(500).json({message:"Internal Sever Error"});
+    }
+}
 
+
+export const logout = async(req,res)=>{
+    try {
+        res.clearCookie("token");
+        return res.status(200).json({message:"Logout Successfully"})
+    } catch (error) {
         return res.status(500).json({message:"Internal Sever Error"});
     }
 }
--- Authentication/backend/routes/user.route.js
+++ Authentication/backend/routes/user.route.js
@@ -1,10 +1,12 @@
 import express, { Router } from "express"
-import {singup } from "../controllers/user.controller.js";
+import {login, logout, singup } from "../controllers/user.controller.js";
 
 
 let router = express(Router());
 
 router.post("/signup",singup);
+router.post("/login",login);
+router.post("/logout",logout);
 
 
 export default router;
